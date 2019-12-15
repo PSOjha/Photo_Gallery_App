b89432cccfd5d958f1589ae1bb5cd54201c6af75
Piyush2769 <startcheckingyourmail@gmail.com>
2019-12-15 10:51:51 +0530

    Working Done

--- app/src/main/java/com/piyushmaheswari/photogalleryapp/AddUpdatePhotosActivity.java
+++ app/src/main/java/com/piyushmaheswari/photogalleryapp/AddUpdatePhotosActivity.java
@@ -1,6 +1,7 @@
 package com.piyushmaheswari.photogalleryapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +9,21 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.piyushmaheswari.photogalleryapp.Database.SqLiteHelper;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -38,6 +46,9 @@ public class AddUpdatePhotosActivity extends AppCompatActivity {
 
     private Uri imageUri;
 
+    private String name,date;
+    private SqLiteHelper sqLiteHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,11 +65,10 @@ public class AddUpdatePhotosActivity extends AppCompatActivity {
         save=findViewById(R.id.saveBtn);
 
 
+        sqLiteHelper=new SqLiteHelper(this);
+
         cameraPermissions=new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
         storagePermissions=new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
-
-
 
         addPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,10 +80,26 @@ public class AddUpdatePhotosActivity extends AppCompatActivity {
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                addData();
             }
         });
 
+    }
+
+    private void addData() {
+        name=""+addName.getText().toString().trim();
+        date=""+addDate.getText().toString().trim();
+
+        String timeStamp=""+System.currentTimeMillis();
+
+        long id=sqLiteHelper.insertPhotos(
+                ""+name,
+                ""+imageUri,
+                ""+date,
+                ""+timeStamp,
+                ""+timeStamp);
+
+        Toast.makeText(this, "Record added against id: "+id, Toast.LENGTH_SHORT).show();
     }
 
     private void imagePickDialog() {
@@ -113,9 +139,24 @@ public class AddUpdatePhotosActivity extends AppCompatActivity {
     }
 
     private void pickFromGallery() {
+
+        Intent galleryIntent=new Intent(Intent.ACTION_PICK);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,IMAGE_PICK_GALLERY);
     }
 
     private void pickFromCamera() {
+
+        ContentValues values=new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"Image title");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"Description");
+
+        imageUri=getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+
+        Intent cameraIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+        startActivityForResult(cameraIntent,IMAGE_PICK_CAMERA);
+
     }
 
     private boolean checkStoragePermission()
@@ -188,5 +229,45 @@ public class AddUpdatePhotosActivity extends AppCompatActivity {
             break;
 
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(resultCode==RESULT_OK)
+        {
+            if(requestCode==IMAGE_PICK_GALLERY)
+            {
+                CropImage.activity(data.getData())
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setAspectRatio(1,1)
+                        .start(this);
+            }
+            else if(requestCode==IMAGE_PICK_CAMERA)
+            {
+                CropImage.activity(imageUri)
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setAspectRatio(1,1)
+                        .start(this);
+            }
+
+            else if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
+            {
+                CropImage.ActivityResult result=CropImage.getActivityResult(data);
+                if(resultCode == RESULT_OK){
+                    Uri resultUri=result.getUri();
+                    imageUri=resultUri;
+
+                    addPhoto.setImageURI(resultUri);
+                }
+                else if(resultCode==CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE)
+                {
+                    Exception exception=result.getError();
+                    Toast.makeText(this, "Error"+exception, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
