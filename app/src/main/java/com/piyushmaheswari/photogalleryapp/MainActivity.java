b743d5951f27b27dc9c959ba4b949ee626e0a2af
Piyush2769 <startcheckingyourmail@gmail.com>
2019-12-15 12:40:53 +0530

    Working Done+2

--- app/src/main/java/com/piyushmaheswari/photogalleryapp/MainActivity.java
+++ app/src/main/java/com/piyushmaheswari/photogalleryapp/MainActivity.java
@@ -1,23 +1,39 @@
 package com.piyushmaheswari.photogalleryapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.piyushmaheswari.photogalleryapp.Adapter.ModelAdapter;
+import com.piyushmaheswari.photogalleryapp.Database.Constants;
+import com.piyushmaheswari.photogalleryapp.Database.SqLiteHelper;
 
 public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton fab;
     private RecyclerView recyclerView;
 
+    private SqLiteHelper sqLiteHelper;
+
+    ActionBar actionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionBar=getSupportActionBar();
+        actionBar.setTitle("All Records");
+        sqLiteHelper=new SqLiteHelper(this);
+        loadRecords();
 
         fab=findViewById(R.id.addPhotos);
 
@@ -30,5 +46,59 @@ public class MainActivity extends AppCompatActivity {
         });
 
         recyclerView=findViewById(R.id.records);
+    }
+
+    private void loadRecords() {
+
+        ModelAdapter modelAdapter=new ModelAdapter(MainActivity.this,
+                sqLiteHelper.getAllRecords(Constants.C_ADDED_TIMESTAMP+ " DESC"));
+
+        recyclerView.setAdapter(modelAdapter);
+        actionBar.setSubtitle("Total: "+sqLiteHelper.getRecordsCount());
+
+    }
+
+    private void searchRecords(String query) {
+        ModelAdapter modelAdapter=new ModelAdapter(MainActivity.this,
+                sqLiteHelper.searchRecords(query));
+
+        recyclerView.setAdapter(modelAdapter);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadRecords();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+
+        MenuItem item=menu.findItem(R.id.action_search);
+        SearchView searchView=(SearchView)item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchRecords(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchRecords(newText);
+                return true;
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/piyushmaheswari/photogalleryapp/RecordDetailActivity.java
@@ -0,0 +1,14 @@
+package com.piyushmaheswari.photogalleryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_detail);
+    }
+}
